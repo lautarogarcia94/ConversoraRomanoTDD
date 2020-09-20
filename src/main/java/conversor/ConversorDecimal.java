8536426c0bcb9b5a2dcf769623aba2ab03507991
Lautaro Garcia <lautygcr@gmail.com>
2020-09-20 15:14:55 -0300

    Refactor del codigo

--- src/main/java/conversor/ConversorDecimal.java
+++ src/main/java/conversor/ConversorDecimal.java
@@ -6,21 +6,17 @@ public class ConversorDecimal implements Conversor {
     @Override
     public String convertir(String numero) {
         numero = numero.toUpperCase();
-        switch (numero){
-            case "IV":
-                return "4";
-            case "IX":
-                return "9";
-            case "XIV":
-                return "14";
-            case "XIX":
-                return "19";
-        }
         return ""+convertirDecena(numero);
     }
 
     private int convertirUnidad(String numero){
         int num = 0;
+        switch (numero) {
+            case "IV":
+                return 4;
+            case "IX":
+                return 9;
+        }
         if(numero.contains("V")){
             num = 5 + (numero.length()-1);
         }else {
@@ -32,7 +28,7 @@ public class ConversorDecimal implements Conversor {
 
     private int convertirDecena(String numero){
         int num = 0;
-        if(numero.contains("X")){
+        if(numero.startsWith("X")){
             num = convertirUnidad(numero.substring(1));
             return num+10 ;
         }else{
