f68068e32549e0d42486e95253239d0c5c527676
Lautaro Garcia <lautygcr@gmail.com>
2020-09-21 11:34:54 -0300

    Se realiza refactor metodo convertirNumero

--- src/main/java/conversor/ConversorDecimal.java
+++ src/main/java/conversor/ConversorDecimal.java
@@ -6,34 +6,33 @@ public class ConversorDecimal implements Conversor {
     @Override
     public String convertir(String numero) {
         numero = numero.toUpperCase();
-        return "" + convertirNumero(numero);
+        return "" + convertirNumero(numero, "");
     }
 
-    private int convertirNumero(String numero) {
-        int num = 0;
-        switch (numero) {
-            case "IV":
-                return 4;
-            case "IX":
-                return 9;
-            case "":
-                return 0;
-        }
+    private int convertirNumero(String numero, String letraAntes) {
 
-        if (numero.startsWith("XL")) {
-            return 40 + convertirNumero(numero.substring(2));
+        if (numero.equals("")) {
+            return 0;
         }
-
         switch (numero.substring(0, 1)) {
             case "L":
-                return 50 + convertirNumero(numero.substring(1));
+                if (letraAntes.equals("X")) {
+                    return 30 + convertirNumero(numero.substring(1), "L");
+                }
+                return 50 + convertirNumero(numero.substring(1), "L");
             case "X":
-                return 10 + convertirNumero(numero.substring(1));
+                if (letraAntes.equals("I")) {
+                    return 8 + convertirNumero(numero.substring(1), "X");
+                }
+                return 10 + convertirNumero(numero.substring(1), "X");
             case "V":
-                return 5 + convertirNumero(numero.substring(1));
+                if (letraAntes.equals("I")) {
+                    return 3 + convertirNumero(numero.substring(1), "V");
+                }
+                return 5 + convertirNumero(numero.substring(1), "V");
             case "I":
-                return 1 + convertirNumero(numero.substring(1));
+                return 1 + convertirNumero(numero.substring(1), "I");
         }
-        return num;
+        return 0;
     }
 }
